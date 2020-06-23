97c656c5fe164a165a241c2bf7cbe5efdb042b1a
SergiiKov <sergiikovtun.vn@gmail.com>
2020-06-23 17:16:08 +0300

    add buttons

--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -5,6 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
 import logo from '../../assets/logo.svg';
@@ -37,6 +38,12 @@ const useStyles = makeStyles(theme => ({
     ...theme.typography.tab,
     minWidth:10,
     marginLeft:'25px'
+  },
+  button: {
+    borderRadius: '50px',
+    marginLeft: '50px',
+    marginRight:'25px',
+    ...theme.typography.estimate
   }
 }))
 
@@ -55,6 +62,7 @@ export default function Header(props) {
                     <Tab className={classes.tab} label='About Us' />
                     <Tab className={classes.tab} label='Contact Us' />
                   </Tabs>
+                  <Button variant="contained" color="secondary" className={classes.button}>Free download</Button>
                   </Toolbar>
             </AppBar>
         </ElevationScroll>
