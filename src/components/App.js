a75658f7cfe8afb9a033ccca846dafe5e0d4bccb
SergiiKov <sergiikovtun.vn@gmail.com>
2020-06-23 12:24:10 +0300

    add component-ui header AppBar Toolbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import Header from '../components/ui/header';
+
 function App() {
   return (
     <div className="App">
-     hi!
+      <Header />
+    <p>hii</p>
     </div>
   );
 }
