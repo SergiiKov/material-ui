1c59e88ec32fdf5c2a52623e4f0e8f8d5ea73428
SergiiKov <sergiikovtun.vn@gmail.com>
2020-06-24 09:19:14 +0300

    add navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import theme from '../components/ui/theme';
-
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/core/styles';
 import Header from '../components/ui/header';
 
@@ -8,7 +8,17 @@ function App() {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-      <Header />
+        <BrowserRouter>
+        <Header />
+        <Switch>
+          <Route exact path = '/' component={()=> <div>home</div>} />
+          <Route exact path = '/services' component={()=> <div>services</div>} />
+          <Route exact path = '/theRevolutions' component={()=> <div>The Revolutions</div>} />
+          <Route exact path = '/aboutUs' component={()=> <div>About Us</div>} />
+          <Route exact path = '/contactUs' component={()=> <div>Contact Us</div>} />
+        </Switch>
+        </BrowserRouter>
+    
       {[...new Array(120)]
             .map(
               () => `Cras mattis consectetur purus sit amet fermentum.
--- src/components/ui/header.jsx
+++ src/components/ui/header.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -9,7 +11,6 @@ import Button from '@material-ui/core/Button';
 import { makeStyles } from '@material-ui/core/styles';
 
 import logo from '../../assets/logo.svg';
-import { useState } from 'react';
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -30,7 +31,13 @@ const useStyles = makeStyles(theme => ({
     marginBottom: '3em'
   },
   logo: {
-    height: "7em"
+    height: "8em"
+  },
+  logoContainer:{
+    padding:0,
+    '&hover': {
+      backgroundColor:'transparent'
+    }
   },
   tabContainer:{
     marginLeft: 'auto'
@@ -55,21 +62,37 @@ export default function Header(props) {
     setValue(value)
   };
 
+  useEffect(()=>{
+    if (window.location.pathname === '/' && value !== 0 ) {
+      setValue(0);
+    } else  if (window.location.pathname === '/services' && value !== 1 ) {
+      setValue(1);
+    } else  if (window.location.pathname === '/theRevolutions' && value !== 2 ) {
+      setValue(2);
+    } else  if (window.location.pathname === '/aboutUs' && value !== 3 ) {
+      setValue(3);
+    } else  if (window.location.pathname === '/contactUs' && value !== 4 ) {
+      setValue(4);
+    }
+  }, [value]);
+
     return(
       <React.Fragment>
         <ElevationScroll>
             <AppBar color='primary'>
                 <Toolbar disableGutters>
-                  <img src={logo} alt='company logo' className={classes.logo} />
+                  <Button component={Link} to='/' className={classes.logoContainer} onClick={()=>setValue(0)} disableRipple>
+                    <img src={logo} alt='company logo' className={classes.logo} />
+                  </Button>
                   <Tabs value={value} onChange={handleChange} className={classes.tabContainer}>
-                    <Tab className={classes.tab} label='Home' />
-                    <Tab className={classes.tab} label='Services' />
-                    <Tab className={classes.tab} label='The Revolutions' />
-                    <Tab className={classes.tab} label='About Us' />
-                    <Tab className={classes.tab} label='Contact Us' />
+                    <Tab className={classes.tab} component={Link} to='/' label='Home' />
+                    <Tab className={classes.tab} component={Link} to='/services' label='Services' />
+                    <Tab className={classes.tab} component={Link} to='/theRevolutions' label='The Revolutions' />
+                    <Tab className={classes.tab} component={Link} to='/aboutUs' label='About Us' />
+                    <Tab className={classes.tab} component={Link} to='/contactUs' label='Contact Us' />
                   </Tabs>
-                  <Button variant="contained" color="secondary" className={classes.button}>Free download</Button>
-                  </Toolbar>
+                    <Button variant="contained" color="secondary" className={classes.button}>Free download</Button>
+                </Toolbar>
             </AppBar>
         </ElevationScroll>
 <div className={classes.ToolbarMargin} />
